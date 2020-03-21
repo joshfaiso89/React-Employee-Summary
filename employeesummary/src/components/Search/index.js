d9588eda559db2de7888b7f773e42325dfbdd6a6
Josh Faison <Joshfaiso@yahoo.com>
2020-03-21 19:22:53 -0400

    completed filtering logic && added a header

--- employeesummary/src/components/Search/index.js
+++ employeesummary/src/components/Search/index.js
@@ -1,4 +1,4 @@
-import React , { Component }from "react";
+import React, { Component } from "react";
 import Table from "../Table";
 // import allEmployees from "../../db/db.json";
 import Api from "../../db/Api";
@@ -14,47 +14,54 @@ class Search extends Component {
     componentDidMount() {
         Api.getRandomUser().then((res) => {
             this.setState({ allEmployees: res.data.results, originalAllEmployees: res.data.results });
-             console.log(res.data.results);
+            //  console.log(res.data.results);
+            const newEmployee = this.state.allEmployees.sort((a, b) => {
+                console.log(a.name.first, b.name.last, "first/last")
+                return a.name.first.toLowerCase() - b.name.first.toLowerCase();
+            })
+
+            let columnName = "first"
+            let newArray = this.state.allEmployees.sort((a, b) => a.name[columnName].localeCompare(b.name[columnName]));
+            this.setState({ allEmployees: newArray });
+
         })
     }
 
     handleInputChange = (event) => {
-        const {name, value} = event.target;
+        const { name, value } = event.target;
         this.setState({
             [name]: value // name: value
         })
 
         console.log(value);
 
-      let newAllEmployees = this.state.allEmployees.filter(person => {
-        //   if(person.name.first.indexOf(value) > -1) {
-        //       return true; 
-        //   }
-        //   else{
-        //       return false;
-        //   }
-          return person.name.first.indexOf(value) > -1;
-      })
-
-      if(value.length === 0) {
-          this.setState({
-              allEmployees: this.state.originalAllEmployees
-          })
-      } else {
-        this.setState({
-            allEmployees: newAllEmployees
+        let newAllEmployees = this.state.allEmployees.filter(person => {
+            return person.name.first.toLowerCase().indexOf(value.toLowerCase()) > -1 || person.name.last.toLowerCase().indexOf(value.toLowerCase()) > -1 || person.email.toLowerCase().indexOf(value.toLowerCase()) > -1 || person.phone.indexOf(value) > -1;
         })
-      }
-   
-       
+
+        if (value.length === 0) {
+            this.setState({
+                allEmployees: this.state.originalAllEmployees
+            })
+        } else {
+            this.setState({
+                allEmployees: newAllEmployees
+            })
+        }
+
+
+
 
     };
 
-
-    render(){
-        return <Table  handleInputChange={this.handleInputChange}   search={this.state.search}  allEmployees={this.state.allEmployees} handleSearch={this.handleSearch} />
+    handleFormSubmit = (columnName) => {
+        let newArray = this.state.allEmployees.sort((a, b) => a.name[columnName].localeCompare(b.name[columnName]));
+        this.setState({ allEmployees: newArray });
     }
-    
+    render() {
+        return <Table handleFormSubmit={this.handleFormSubmit} handleInputChange={this.handleInputChange} search={this.state.search} allEmployees={this.state.allEmployees} handleSearch={this.handleSearch} />
+    }
+
 
 }
 
--- employeesummary/src/components/Table/index.js
+++ employeesummary/src/components/Table/index.js
@@ -6,16 +6,19 @@ function Table(props) {
     return (
 
         <div className="container">
+            <div class="alert alert-dark" role="alert" style={{color: "white", backgroundColor: "#212529", textAlign: "center", fontSize: "20px"}}>
+                Search Employees by First or Last Name
+            </div>
             <div className="text-center">
-            <InputField  handleInputChange={props.handleInputChange}  searchValue={props.search} />
+                <InputField handleInputChange={props.handleInputChange} searchValue={props.search} />
             </div>
             <br></br>
             <table className="table table-hover">
-                <thead className="thead-dark"> 
+                <thead className="thead-dark">
                     <tr>
                         <th scope="col">Picture</th>
-                        <th scope="col">First Name</th>
-                        <th scope="col">Last Name</th>
+                        <th scope="col" onClick={() => { props.handleFormSubmit("first") }}  >First Name</th>
+                        <th scope="col" onClick={() => { props.handleFormSubmit("last") }}   >Last Name</th>
                         <th scope="col">Email</th>
                         <th scope="col">Phone</th>
                     </tr>
